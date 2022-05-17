045d368c8a2b7a337d9f861e9003708cc733db94
trynosmzondo <trynosm68@gmail.com>
2022-05-17 11:55:16 -0700

    my commit

--- string.c
+++ string.c
@@ -3,40 +3,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-
-/**
- * make_string - creates formatted string
- * @s: format
- * @vl: argument
- * Return: formatted string
- */
-
-char *make_string(char *s, va_list vl)
-{
-	int p, w, slen, mlen, flen, j, k;
-	char *result, *string;
-
-	slen = mlen = j = k = 0;
-	string = va_arg(vl, char *);
-	if (string == NULL)
-		exit(98);
-	p = give_precision(s, 's');
-	w = give_width(s, 's');
-	slen = _strlen(string);
-	flen = _strlen(s);
-	/* determine malloc len */
-	if (w > p && w != 0 && p != -1)
-		mlen = w;
-	else if (p > w && p != 0)
-		mlen = p;
-	else
-		mlen = slen;
-	result = malloc(sizeof(char) * (mlen + 1));
-	result = get_result(result, string, p, w, slen, flen, mlen);
-	return (result);
-}
-
-
 /*for strings*/
 /**
  * conversion_string - check if format is right
